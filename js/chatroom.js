f6bc01d35dad9f503cf97ebfb1293c853b0b610e
tavisca-dpingale <dpingale@tavisca.com>
2019-02-21 11:51:53 +0530

    user initials in picture area, user list search functionality

--- js/chatroom.js
+++ js/chatroom.js
@@ -2,9 +2,8 @@
 
 var userListItem = document.getElementsByClassName("user-list-item");
 var chatBoxItem = document.getElementsByClassName("chat-box");
-
+var userItem = document.getElementsByClassName("user-item");
 var searchUserInput = document.getElementById("userListSearchBox");
-
 
 
 // SUBMIT SIGNUP APP
@@ -15,7 +14,7 @@ function signOutAction(){
 // GET USER INITIALS AND ADD FIRST LETTER IN CIRCLE FOR - USER LIST
 function getApplyUserListInitials(){
     for(let i = 0; i < userListItem.length; i++) {
-        userListItem[i].firstElementChild.innerText = userListItem[i].innerText.substring(0,1).toUpperCase();
+        userListItem[i].firstElementChild.innerText = userListItem[i].innerText.substring(0,2).toUpperCase();
      }
 }
 
@@ -23,19 +22,35 @@ getApplyUserListInitials();
 
 // GET USER INITIALS AND ADD FIRST LETTER IN CIRCLE FOR - CHAT BOX
 function getApplyUserChatInitials(){
-     for(let s = 0 ; s < chatBoxItem.length; s++){
+     for(let s = 0; s < chatBoxItem.length; s++){
         let userDataValue = chatBoxItem[s].getAttribute("data-user");
-        chatBoxItem[s].firstElementChild.innerText = userDataValue.substring(0,1).toUpperCase();
+        chatBoxItem[s].firstElementChild.innerText = userDataValue.substring(0,2).toUpperCase();
      }
 }
 
 getApplyUserChatInitials();
 
+// HIDE USER LIST INITIALLY
+function hideUserList() {
+    for(let u = 0; u < userItem.length; u++){
+        userItem[u].style.display = "none";
+    }
+}
+
+hideUserList();
+
 // USER SEARCH FROM LIST
 function searchUserAction() {
-    let username = document.searchUserForm["searchUserName"];
-    let typedName = username.value;
+    let userInitials = document.searchUserForm["searchUserName"];
+    var typedText = userInitials.value;
 
-    alert(typedName);
+    for(let i = 0; i < userItem.length; i++){
+        let txtValue = userItem[i].innerText;
 
+        if(txtValue.toLowerCase().indexOf(typedText) > 0 || txtValue.toUpperCase().indexOf(typedText) > 0) {
+            userItem[i].style.display = "block";
+        } else {
+            userItem[i].style.display = "none";
+        }
+    }
 }
